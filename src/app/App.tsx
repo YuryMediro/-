3d8399b7b1409815b494e3c95c332e3d08e3e2c8
Yury Mediro <sagn375@gmail.com>
2025-04-01 11:29:49 +0300

    customHook

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,48 +1,13 @@
-import { useEffect, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+import { useState } from 'react'
 import { TaskInput } from '../components/TaskInput'
 import s from './App.module.css'
 import { TaskList } from '../components/TaskList'
 import { Footer } from '../components/Footer'
-
-type Task = {
-	id: string
-	text: string
-	completed: boolean
-}
+import { useTasks } from '../hooks/useTask'
 
 export const App = () => {
-	const [tasks, setTasks] = useState<Task[]>(() => {
-		const storedTasks = localStorage.getItem('tasks')
-		return storedTasks ? JSON.parse(storedTasks) : []
-	})
+	const { tasks, addTask, toggleTask, deleteTask, clearCompleted } = useTasks()
 	const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all')
-
-	useEffect(() => {
-		localStorage.setItem('tasks', JSON.stringify(tasks))
-	}, [tasks])
-	
-
-	const addTask = (text: string) => {
-		const newTask: Task = { id: uuidv4(), text, completed: false }
-		setTasks(prev => [...prev, newTask])
-	}
-
-	const toggleTask = (id: string) => {
-		setTasks(prev =>
-			prev.map(task =>
-				task.id === id ? { ...task, completed: !task.completed } : task
-			)
-		)
-	}
-
-	const deleteTask = (id: string) => {
-		setTasks(prev => prev.filter(task => task.id !== id))
-	}
-
-	const clearCompleted = () => {
-		setTasks(prev => prev.filter(task => !task.completed))
-	}
 	const filteredTasks = tasks.filter(task => {
 		if (filter === 'active') return !task.completed
 		if (filter === 'completed') return task.completed
